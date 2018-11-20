d7dcff83d28c686bda242d25f251bcc2ccbb1255
Bryce Jech <bryce@brycejech.com>
2018-11-20 14:13:54 -0600

    add generic form handler, convert to ES6

--- client/js/main.js
+++ client/js/main.js
@@ -1,227 +1,208 @@
 'use strict';
 
-// Handle url form submit
-(function(){
-    var linkSubmit  = document.querySelector('.link-submit'),
-        linkInput   = document.querySelector('.link-input'),
-        qrContainer = document.querySelector('.url-img-container');
+// Generic form handler
+(function() {
 
-    linkSubmit.addEventListener('click', function(e){
-        e.preventDefault();
-
-        ajax({
-            url: '/api/v1/url',
-            method: 'PUT',
-            dataType: 'json',
-            data: JSON.stringify({ url: linkInput.value }),
-            success: function(data){
-
-                // Clear contents
-                qrContainer.innerHTML = '';
-
-                // Full-screen link
-                var imgAnchor = document.createElement('a');
-                imgAnchor.innerText = 'View Fullscreen';
-                imgAnchor.setAttribute('target', '_blank');
-                imgAnchor.setAttribute('rel', 'noopener');
-                imgAnchor.href = data.svgUrl;
-                qrContainer.appendChild(imgAnchor);
-
-                // Display the QR image
-                var img = document.createElement('img');
-                img.src = data.svgUrl;
-                qrContainer.appendChild(img);
-
-                // Add a link to the submitted URL
-                var urlAnchor = document.createElement('a');
-                urlAnchor.innerText = data.data;
-                urlAnchor.setAttribute('target', '_blank');
-                urlAnchor.setAttribute('rel', 'noopener');
-                urlAnchor.href = data.data;
-                qrContainer.appendChild(urlAnchor);
-            },
-            error: function(xhr, status, err){
-                console.error(status, err);
-            }
-        });
-
-    });
-})();
-
-// Handle contact form submit
-(function(){
-    var vCardForm   = document.getElementById('vcard-form'),
-        submitBtn   = vCardForm.querySelector('.vCard-submit'),
-        qrContainer = document.querySelector('.vcard-img-container');
-
-    submitBtn.addEventListener('click', function(e){
-        e.preventDefault();
-
-        ajax({
-            url: '/api/v1/vcard',
-            method: 'PUT',
-            dataType: 'json',
-            data: JSON.stringify(getFormData()),
-            success: function(data){
-
-                // Clear contents
-                qrContainer.innerHTML = '';
-
-                // Full-screen link
-                var imgAnchor = document.createElement('a');
-                imgAnchor.innerText = 'View Fullscreen';
-                imgAnchor.setAttribute('target', '_blank');
-                imgAnchor.setAttribute('rel', 'noopener');
-                imgAnchor.href = data.svgUrl;
-                qrContainer.appendChild(imgAnchor);
-
-                // Display QR image
-                var img = document.createElement('img');
-                img.src = data.svgUrl;
-                qrContainer.appendChild(img);
-            },
-            error: function(xhr, status, err){
-                console.error(status, err);
-            }
-        });
-    });
-
-    var inputs = [
-        // Name
-        'firstName', 'lastName','namePrefix','nameSuffix','nickname',
-        // Org
-        'organization','title','website',
-        // Emails
-        'email','workEmail',
-        // Phones
-        'cellPhone','workPhone','homePhone','pager',
-        // Faxes
-        'homeFax','workFax',
-        // Dates
-        'birthday','anniversary',
-        //Notes
-        'notes'
+    const forms = [
+        {
+            title: 'Email Form',
+            selector: '#email-form',
+            imgContainerSelector: '.email-img-container'
+        },
+        {
+            title: 'URL Form',
+            selector: '#url-form',
+            imgContainerSelector: '.url-img-container'
+        },
+        {
+            title: 'vCard Form',
+            selector: '#vcard-form',
+            imgContainerSelector: '.vcard-img-container'
+        },
+        {
+            title: 'SMS Form',
+            selector: '#sms-form',
+            imgContainerSelector: '.sms-img-container'
+        },
+        {
+            title: 'Phone Form',
+            selector: '#phone-form',
+            imgContainerSelector: '.phone-img-container'
+        }
     ];
 
-    function getFormData(){
-        var data = {}
+    forms.forEach(function(descriptor){
+        const form = document.querySelector(descriptor.selector);
 
-        inputs.forEach(function(name){
-            var el = vCardForm.querySelector('.' + name);
+        form.addEventListener('submit', _getSubmitHandler(descriptor));
+    });
 
-            data[name] = el.value;
-        });
+    // Keeps a reference to the original form descriptor
+    function _getSubmitHandler(formDescriptor){
 
-        return data;
-    }
-})();
+        return submitHandler;
 
-// Handle email submit
-(function(){
-    var emailForm   = document.getElementById('email-form'),
-        emailSubmit = emailForm.querySelector('.email-submit'),
-        qrContainer = document.querySelector('.email-img-container');
+        function submitHandler(e){
+            e.preventDefault();
 
-    var inputClasses = ['email', 'subject', 'body'];
+            let hasErrors = false;
 
-    function formSubmit(e){
-        e.preventDefault();
+            const inputs = Array.from(this.querySelectorAll('input, textarea, select')),
+                  data   = {};
 
-        var data = {};
+            inputs
+                .filter(input => input.getAttribute('type') !== 'submit')
+                .forEach(input => {
 
-        inputClasses.forEach(function(cls){
-            var el = emailForm.querySelector('.' + cls);
+                    const value = _getInputValue(input),
+                          name  = input.getAttribute('name');
 
-            if(el && el.value){
-                data[cls] = el.value
+                    // Input value must be validated
+                    if(!!input.getAttribute('data-validator')){
+                        console.log('has data-validator');
+                        const validator = new RegExp(input.getAttribute('data-validator'));
+
+                        if(!validator.test(value)){
+                            console.log('validation failed');
+                            // Validation failed
+                            hasErrors = true;
+                            input.classList.remove('success');
+                            input.classList.add('error');
+                            input.nextElementSibling.innerText = input.getAttribute('data-error');
+                        }
+                        else{
+                            console.log('validation passed');
+                            // Validation passed
+                            input.classList.remove('error');
+                            input.classList.add('success');
+                            input.nextElementSibling.innerText = '';
+
+                            data[name] = value;
+                        }
+                    }
+
+                    if(value){
+                        data[name] = value;
+                    }
+                });
+
+            if(!hasErrors){
+                const url    = this.getAttribute('action'),
+                      method = this.getAttribute('method');
+
+                if(!(url && method)){
+                    return console.error(`Failed to lookup url or method for form "${ form.title }"`);
+                }
+
+                ajax({
+                    url:       url,
+                    method:    method,
+                    dataType: 'json',
+                    data:      JSON.stringify(data),
+                    success: (response) => {
+
+                        const qrContainer = document.querySelector(formDescriptor.imgContainerSelector);
+
+                        // Clear contents
+                        qrContainer.innerHTML = '';
+
+                        // Full-screen link
+                        const imgAnchor = document.createElement('a');
+                        imgAnchor.innerText = 'View Fullscreen';
+                        imgAnchor.setAttribute('target', '_blank');
+                        imgAnchor.setAttribute('rel', 'noopener');
+                        imgAnchor.href = response.svgUrl;
+                        qrContainer.appendChild(imgAnchor);
+
+                        // Display the QR image
+                        const img = document.createElement('img');
+                        img.src = response.svgUrl;
+                        qrContainer.appendChild(img);
+                    },
+                    error: (xhr, status, err) => {
+                        console.error(`Error submitting data to url "${ formDescriptor.title }"`);
+
+                    }
+                });
             }
-        });
-
-        ajax({
-            url: '/api/v1/email',
-            method: 'PUT',
-            dataType: 'json',
-            data: JSON.stringify(data),
-            success: function(data){
-
-                // Clear contents
-                qrContainer.innerHTML = '';
-
-                // Full-screen link
-                var imgAnchor = document.createElement('a');
-                imgAnchor.innerText = 'View Fullscreen';
-                imgAnchor.setAttribute('target', '_blank');
-                imgAnchor.setAttribute('rel', 'noopener');
-                imgAnchor.href = data.svgUrl;
-                qrContainer.appendChild(imgAnchor);
-
-                // Display the QR image
-                var img = document.createElement('img');
-                img.src = data.svgUrl;
-                qrContainer.appendChild(img);
-            },
-            error: function(xhr, status, err){
-                console.error(status, err);
-            }
-        })
-    }
-
-    emailSubmit.addEventListener('click', formSubmit);
-})();
-
-// Handle SMS submit
-(function(){
-
-    var smsForm     = document.getElementById('sms-form-container'),
-        smsSubmit   = smsForm.querySelector('.sms-submit'),
-        qrContainer = document.querySelector('.sms-img-container');
-
-    function formSubmit(e){
-        e.preventDefault();
-
-        var data = {},
-            inputClasses = ['number', 'body'];
-
-
-        inputClasses.forEach(function(cls){
-            var el = document.querySelector('.' + cls);
-
-            if(el && el.value){
-                data[cls] = el.value;
-            }
-        });
-
-        ajax({
-            url: '/api/v1/sms',
-            method: 'PUT',
-            dataType: 'json',
-            data: JSON.stringify(data),
-            success: function(data){
-                // Clear contents
-                qrContainer.innerHTML = '';
-
-                // Full-screen link
-                var imgAnchor = document.createElement('a');
-                imgAnchor.innerText = 'View Fullscreen';
-                imgAnchor.setAttribute('target', '_blank');
-                imgAnchor.setAttribute('rel', 'noopener');
-                imgAnchor.href = data.svgUrl;
-                qrContainer.appendChild(imgAnchor);
-
-                // Display the QR image
-                var img = document.createElement('img');
-                img.src = data.svgUrl;
-                qrContainer.appendChild(img);
-            },
-            error: function(xhr, status, err){
-                console.error(status, err);
-            }
-        });
+        }
     }
 
-    smsSubmit.addEventListener('click', formSubmit);
+    // Allows use of HTMLElement.matches() back to IE9
+    if (!Element.prototype.matches) {
+    	Element.prototype.matches = Element.prototype.msMatchesSelector || Element.prototype.webkitMatchesSelector;
+    }
 
-})();
+    // Find parent element by selector or callback
+    function _parents(el, cb){
+        const str = cb;
+        typeof cb === 'string' && (cb = (el) => el.matches(str));
+        (() => {})();
+        if( cb(el) ) return el;
+
+        // Traverse up the DOM
+        while(el && el.parentNode !== document){
+            el = el.parentNode;
+            if( cb(el) ) return el;
+        }
+        return undefined;
+    }
+
+    // Get value for various input types
+    function _getInputValue(el){
+        const type = el.getAttribute('type') ? el.getAttribute('type').toLowerCase() : null,
+              name = el.getAttribute('name');
+
+        let parentForm;
+
+        switch(type){
+            case 'text':
+            case 'password':
+            case 'submit':
+            case 'reset':
+            case 'button':
+            case 'email':
+            case 'number':
+            case 'search':
+            case 'color':
+            case 'date':
+            case 'month':
+            case 'range':
+            case 'tel':
+            case 'time':
+            case 'url':
+            case 'week':
+                return el.value;
+
+            case 'radio':
+                parentForm = _parents(el, 'form');
+
+                if(!parentForm) return;
+
+                const checked = parentForm.querySelector(`input[name="${ name }"]:checked`);
+
+                return checked ? checked.value : '';
+
+            case 'checkbox':
+                parentForm = _parents(el, 'form');
+
+                if(!parentForm) return;
+
+                const checkboxes = Array.from(parentForm.querySelectorAll(`input[name="${ name }"]`)),
+                      values     = [];
+
+                checkboxes.forEach(ck => {
+                    if(ck.checked) values.push(ck.value);
+                });
+
+                return values.join(', ');
+
+            default:
+                return el.value;
+        }
+    }
+
+}());
 
 
 // Get all submitted data and display it
@@ -234,13 +215,13 @@
         { url: '/api/v1/sms',   containerId: 'smss'   },
         { url: '/api/v1/phone', containerId: 'phones' }
     ]
-    .forEach(function(endpoint){
+    .forEach(endpoint => {
         ajax({
             url: endpoint.url,
             method: 'GET',
             dataType: 'json',
             success: function(data){
-                var html = ''
+                let html = ''
                 data.forEach(function(item){
                     html += `
                         <p>
@@ -249,7 +230,8 @@
                         </p>
                     `;
                 });
-                var container = document.getElementById(endpoint.containerId)
+
+                const container = document.getElementById(endpoint.containerId)
 
                 if(container){
                     container.innerHTML = html;
@@ -271,42 +253,36 @@
 // Handle pane switching
 (function(){
     // Containers
-    var paneContainer = document.getElementById('pane-container');
+    const paneContainer = document.getElementById('pane-container');
 
     // Buttons
-    var urlPaneBtn   = document.getElementById('url-pane-btn'),
-        vCardPaneBtn = document.getElementById('vcard-pane-btn'),
-        emailPaneBtn = document.getElementById('email-pane-btn'),
-        smsPaneBtn   = document.getElementById('sms-pane-btn'),
-        phonePaneBtn = document.getElementById('phone-pane-btn');
+    const urlPaneBtn   = document.getElementById('url-pane-btn'),
+          vCardPaneBtn = document.getElementById('vcard-pane-btn'),
+          emailPaneBtn = document.getElementById('email-pane-btn'),
+          smsPaneBtn   = document.getElementById('sms-pane-btn'),
+          phonePaneBtn = document.getElementById('phone-pane-btn');
 
-    var buttons = [ urlPaneBtn, vCardPaneBtn, emailPaneBtn, smsPaneBtn, phonePaneBtn ];
+    const buttons = [urlPaneBtn, vCardPaneBtn, emailPaneBtn, smsPaneBtn, phonePaneBtn];
 
     function showPane(e){
-        var targetPane = this.getAttribute('data-target');
+        const targetPane = this.getAttribute('data-target');
 
         if(!targetPane) return;
 
-        ([].slice.call(paneContainer.querySelectorAll('.pane')))
-            .forEach(function(pane){
-                pane.classList.remove('active');
-            });
+        Array.from(paneContainer.querySelectorAll('.pane'))
+            .forEach( pane => pane.classList.remove('active') );
 
-        var pane = document.querySelector(targetPane);
+        const pane = document.querySelector(targetPane);
 
         if(pane){
             pane.classList.add('active');
 
-            buttons.forEach(function(button){
-                button.classList.remove('active');
-            });
+            buttons.forEach( btn => btn.classList.remove('active') );
 
             this.classList.add('active');
         }
     }
 
-    buttons.forEach(function(btn){
-        btn.addEventListener('click', showPane);
-    });
+    buttons.forEach( btn => btn.addEventListener('click', showPane) );
 
 })();
