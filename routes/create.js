2ff24bca7f68d0076b8c1ef6940f8f7adc67afc5
Bryce Jech <bryce@brycejech.com>
2018-11-20 08:51:43 -0600

    add some input checking

--- routes/create.js
+++ routes/create.js
@@ -24,6 +24,7 @@ module.exports = function urlRoutes(express){
         }
     });
 
+
     // Create a QR code containing a vCard
     router.put('/api/v1/vcard', async (req, res, next) => {
         try{
@@ -76,7 +77,12 @@ module.exports = function urlRoutes(express){
     });
 
 
+    // Create a QR code that sends an email
     router.put('/api/v1/email', async (req, res, next) => {
+        if(!req.body.email){
+            return res.status(400).send({ message: 'Must provide an email address' });
+        }
+        
         try{
             const result = await qrBuddy.email(req.body.email, req.body.subject, req.body.body);
 
@@ -88,7 +94,13 @@ module.exports = function urlRoutes(express){
         }
     });
 
+
+    // Create a QR code that sends an SMS text
     router.put('/api/v1/sms', async (req, res, next) => {
+        if(!req.body.number){
+            return res.status(400).json({ message: 'Must provide a phone number' });
+        }
+
         try{
             const result = await qrBuddy.sms(req.body.number, req.body.body);
 
